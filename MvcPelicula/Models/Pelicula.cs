d9395efccaedba5d04d6d98b2b81d13300d444a8
kkentes <kevin.fuentesk@gmail.com>
2022-08-22 18:28:44 -0600

    Ejercicio1_G2

---
+++ MvcPelicula/Models/Pelicula.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcPelicula.Models
+{
+    public class Pelicula
+    {
+        public int ID { get; set; }
+        public string Titulo { get; set; }
+        [Display(Name = "Fecha de Lanzamiento")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode
+= true)]
+        public DateTime FechaLanzamiento { get; set; }
+        public string Genero { get; set; }
+        public string Precio { get; set; }
+    }
+    public class PeliculaDBContext : DbContext
+    {
+        public DbSet<Pelicula> Peliculas { get; set; }
+    }
+}
---
+++ dbo.Table.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY (1, 1), 
+    [titulo] VARCHAR(60) NOT NULL, 
+    [fechaLanzamiento] DATETIME NOT NULL, 
+    [genero] VARCHAR(50) NOT NULL, 
+    [precio] VARCHAR(50) NOT NULL
+)
